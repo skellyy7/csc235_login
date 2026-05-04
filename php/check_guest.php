ec7450503d05c33c42ddf4dd6dfa9d512c658187
Brendan Palazzo <258644617+skellyy7@users.noreply.github.com>
2026-05-03 21:43:50 -0400

    Change guest user role check from 2 to 3

--- php/check_guest.php
+++ php/check_guest.php
@@ -8,9 +8,9 @@
  * @date      2026-02-24
  * @version   1.0
  */
-// Check if user role is 2 (guest user)
+// Check if user role is 3 (guest user)
 
-if($user_role == 2){
+if($user_role == 3){
   // Everything is working
   // See Console Output
   echo '<script>console.log("Everything is Working");</script>';
